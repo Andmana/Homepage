0702d7efe781eb912daa5ee48fef3a60ea6f095b
andmana <iam.andmana@gmail.com>
2025-03-03 17:32:49 +0700

    Apply theme button event

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
+const themeBtn = document.querySelector(".theme-button");
+const themeIndicator = document.querySelector(".theme-indicator");
+
+themeBtn.addEventListener("click", () => {
+    document.documentElement.classList.toggle("dark");
+    themeIndicator.classList.toggle("dark");
+});
+
 document.addEventListener("DOMContentLoaded", () => {
     const prefersDark = window.matchMedia(
         "(prefers-color-scheme: dark)"
     ).matches;
     if (prefersDark) {
         document.documentElement.classList.add("dark");
-        document.querySelector(".theme-indicator").classList.toggle("dark");
+        themeIndicator.classList.toggle("dark");
     }
 });
